10ed7fa04bd7a94d69e2d0a6b5161bf253b2ff61
Akhil-kukku <Akhil-kukku@users.noreply.github.com>
2026-04-09 13:40:51 +0000

    deploy: ab69f3a69d419e5c3abf60e33fc69c6a9aae92c3

---
+++ _next/static/xLpBNqoBK-UjEvKqwktuz/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/I4E-ROI-Calculator//_next/:path+",
+        "destination": "/I4E-ROI-Calculator/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
